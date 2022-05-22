dad6bc54cac9b23e34c9da4dece9adfb4de49cf8
babylonthemes <babylonthemes@gmail.com>
2022-05-22 04:39:31 +0300

    First commit

---
+++ css.bat
@@ -0,0 +1,1 @@
+npx tailwindcss -i ./src/css/input.css -o ./src/css/dist/output.css --minify --watch
---
+++ src/js/boxed/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    // home boxed-layout slider 
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:false,
+        rtl:true,
+        items:1,
+    })
+
+    AOS.init();
+});
+
+
